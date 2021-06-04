8af31ebc0694604df53c798059105e8be583dd7a
Zellon19 <axel.alvarez2k4@outlook.es>
2021-06-04 18:58:20 -0300

    update de lo anterior

---
+++ Components/Empresa.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Empresa extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Información de una empresa exprimidora de clase baja pero nadie la quiere así que no te doy la info </Text>
+            </View>
+        )
+    }
+}
