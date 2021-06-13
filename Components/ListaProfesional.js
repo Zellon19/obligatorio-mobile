2798e9cfe4642c250618dd587955232f4facd9f2
Zellon19 <axel.alvarez2k4@outlook.es>
2021-06-13 20:41:14 -0300

    style lista empresa, profesional, rubro

--- Components/ListaProfesional.js
+++ Components/ListaProfesional.js
@@ -5,6 +5,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#10f4f3',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
     }
 })
 
@@ -12,8 +21,8 @@ const data = require('../info/profesionales.json');
 
 export default function ListaEmpresa({navigation}) {
     const renderItem = ({item}) => (
-        <TouchableOpacity onPress={() => navigation.navigate('Profesional', item)}>
-            <Text> {item.name}</Text>
+        <TouchableOpacity style={styles.item}onPress={() => navigation.navigate('Profesional', item)}>
+            <Text style={styles.title}> {item.name}</Text>
         </TouchableOpacity>
     );
     return (
