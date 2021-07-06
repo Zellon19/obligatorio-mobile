1fb4cb531ccff07b2f387eb297731d9153224467
Zellon19 <axel.alvarez2k4@outlook.es>
2021-07-06 20:49:03 -0300

    rubro link a empresa

--- Components/Rubro.js
+++ Components/Rubro.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+
+//mostrar empresas que pertenecen al rubro                  
+
+const empresas = require('../info/empresas.json');
 
 export default function Rubro(pItem){
-
+    const navigation = pItem.navigation;
     const item = pItem.navigation.state.params;
+    const filtered = [];
+    empresas.forEach(empItem => {
+        if(empItem.rubro === item.id)
+            filtered.push(empItem);
+    })
+    const renderItem = (rItem) => (
+        <TouchableOpacity style={styles.item} onPress={() => { navigation.navigate('Empresa', rItem.item)}}>
+            <Text> {rItem.item.nombre} </Text>
+        </TouchableOpacity>
+    );
     return (
-        <View>
-            <Text> {item.name} {"\n"} aquí se encontrará una lista de las empresas de este rubro </Text>
+        <View style={styles.container}>
+            <Text> Empresas de {item.name} </Text>
+            {filtered ? <FlatList data={filtered} renderItem={renderItem} keyExtractor={item => item.id}/> : <Text>No hay empresas</Text>}
             
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        padding: 20,
+        marginVertical: 4,
+        marginHorizontal: 16,
+        borderRadius: 15,
+        shadowColor: 'rgba(0,0,0, .4)', // IOS
+        shadowOffset: { height: 1, width: 1 }, // IOS
+        shadowOpacity: 6, // IOS
+        shadowRadius: 1, //IOS     
+        elevation: 2, // Android
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: '#ccaacc', 
+        paddingTop: 50,
+    },
+})
